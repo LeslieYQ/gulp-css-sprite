46b2d397c5d28deccb5fdd0e3821d91a94f7a898
yuqiu <yuqiu_xa@netposa.com>
2014-12-05 14:08:38 +0800

    修改信息
    npm publish了

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,25 +3,29 @@
  * @authors yuqiu (yuqiu_xa@netposa.com)
  * @date    2014-11-24 15:32:13
  * @version $Id$
+ *
+ * 需要自行安装：
+ * npm install through-gulp thenjs.
+ * 
  */
 
 var gulp = require('gulp');
-var gulpCssSprite = require('gulp-css-sprite');
+var gulpCssSprite = require('./lib/index.js');
 var path = require('path');
 var through = require('through-gulp');
 var thenjs = require('thenjs');
 var sourceCss = [];
 
 
-gulp.task('default', function() {
-	gulp.src('src/module/{,**/}sprite.css').pipe(gulp.dest('tmp/test/'));
+gulp.task('default', ['example'], function() {
+	console.log('success!!!');
 });
 
 
 // 获取所有符合规则的css路径数组
-gulp.task('test', function() {
+gulp.task('source', function() {
 	sourceCss = [];
-	var stream = gulp.src('src/module/{,**/}sprite.css')
+	var stream = gulp.src('example/{,**/}sprite.css')
 		.pipe(gulpCssSprite.map())
 		.pipe(through.map(function(file) {
 			sourceCss = file.contents.toString().split(',');
@@ -32,7 +36,7 @@ gulp.task('test', function() {
 
 
 // 把所有需要合并图片的css 处理一遍，依赖上一步的路径地址
-gulp.task('sample', ['test'], function() {
+gulp.task('example', ['source'], function() {
 	thenjs.eachSeries(sourceCss, function(cont, value, index, array) {
 		gulp.src(value)
 			.pipe(gulpCssSprite.sprite({
---
+++ test/test.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @authors yuqiu (yuqiu_xa@netposa.com)
+ * @date    2014-12-05 13:51:07
+ * @version $Id$
+ */
+
+'use strict';
